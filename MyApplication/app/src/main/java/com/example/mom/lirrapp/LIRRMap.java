d9e64cc58d0a062529d5abe18adae3cc2ffee31f
Ella Williams <MOM@Rashads-iMac.lan>
2016-05-17 15:45:03 -0400

    Fixed Location, added Icon to mark current Location

--- MyApplication/app/src/main/java/com/example/mom/lirrapp/LIRRMap.java
+++ MyApplication/app/src/main/java/com/example/mom/lirrapp/LIRRMap.java
@@ -3,12 +3,26 @@ package com.example.mom.lirrapp;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.constants.MyLocationTracking;
@@ -21,12 +35,22 @@ import com.mapbox.mapboxsdk.maps.widgets.UserLocationView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LIRRMap extends AppCompatActivity {
+public class LIRRMap extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     private MapView mapView;
     private static final int PERMISSIONS_LOCATION = 0;
     private MapboxMap map1;
-    private UserLocationView userLocationView;
+    private static final String TAG= LIRRMap.class.getSimpleName();
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST= 1000;
+    private Location mLastLocationCoordinates;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mRequestLocationUpdates = false;
+    private LocationRequest mLocationRequest;
+    private static int UPDATE_INTERVAL= 10000;
+    private static int FASTEST_INTERVAL= 5000;
+    private static int DISPLACEMENT= 10;
+    double lon;
+    double lat;
 
 
     @Override
@@ -44,6 +68,10 @@ public class LIRRMap extends AppCompatActivity {
             public void onMapReady(MapboxMap mapboxMap) {
 
                 myLocation();
+                IconFactory iconFactory = IconFactory.getInstance(LIRRMap.this);
+                Drawable iconDrawable = ContextCompat.getDrawable(LIRRMap.this, R.drawable.purple_marker);
+                Icon icon = iconFactory.fromDrawable(iconDrawable);
+                mapboxMap.addMarker(new MarkerOptions().title("Current Location").position(new LatLng(lat,lon)).icon(icon));
 
                 drawPolyLinesBabylon(mapboxMap);
                 ronkonkomaTrainPolyline(mapboxMap);
@@ -64,13 +92,157 @@ public class LIRRMap extends AppCompatActivity {
                 hempsteadBranchMarkers(mapboxMap);
                 hempsteadBranchPolyline(mapboxMap);
 
-//                // Customize map with markers, polylines, etc.
-//                mapboxMap.addMarker(new MarkerOptions()
-//                .position(new LatLng(40.680263, -73.420472)).title("Amityville Train Station"));
-
 
             }
         });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(LIRRMap.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET
+            }, 10 );
+
+            return;
+
+        }else{
+            configureButton();
+
+        }
+
+
+
+
+
+        if(checkPlayServices() ){
+            buildGoogleApiClient();
+            createLocationRequest();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(mGoogleApiClient !=null){
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+
+        checkPlayServices();
+        if(mGoogleApiClient.isConnected() && mRequestLocationUpdates){
+            startLocationUpdates();
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+        stopLocationUpdates();
+
+    }
+
+    private void displayLocation(){
+        mLastLocationCoordinates= LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocationCoordinates !=null){
+            lat= mLastLocationCoordinates.getLatitude();
+            lon= mLastLocationCoordinates.getLongitude();
+
+            Toast.makeText(LIRRMap.this, " ", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(LIRRMap.this, "Couldn't get the location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void togglePeriodLocation(){
+        if(!mRequestLocationUpdates){
+            mRequestLocationUpdates=true;
+            startLocationUpdates();
+        }else{
+            mRequestLocationUpdates= false;
+
+            stopLocationUpdates();
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiClient= new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    protected void createLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+
+    }
+
+    private boolean checkPlayServices(){
+        int resultCode= GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(resultCode != ConnectionResult.SUCCESS){
+            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)){
+                GooglePlayServicesUtil.getErrorDialog(resultCode,this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }else{
+                Toast.makeText(LIRRMap.this, "This device is not supported", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    protected void startLocationUpdates(){
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    protected void stopLocationUpdates(){
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+        }
+    }
+
+    private void configureButton() {
+//        mLocation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+//                Toast.makeText(LocationPage.this, "Currently getting your location", Toast.LENGTH_LONG).show();
+//                myDate.setLon(lon);
+//                myDate.setLat(lat);
+//                Intent intent = new Intent(LocationPage.this, VenueType.class);
+//                intent.putExtra(MyDateItems.MY_ITEMS, myDate);
+//                startActivity(intent);
+//
+//            }
+//        });
+
     }
 
 
@@ -360,17 +532,7 @@ public class LIRRMap extends AppCompatActivity {
     }
 
             // Add the mapView lifecycle to the activity's lifecycle methods
-            @Override
-            public void onResume() {
-                super.onResume();
-                mapView.onResume();
-            }
 
-            @Override
-            public void onPause() {
-                super.onPause();
-                mapView.onPause();
-            }
 
             @Override
             public void onLowMemory() {
@@ -392,15 +554,40 @@ public class LIRRMap extends AppCompatActivity {
 
 
 
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode) {
+//            case PERMISSIONS_LOCATION: {
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    map1.setMyLocationEnabled(true);
+//                }
+//            }
+//        }
+//    }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_LOCATION: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    map1.setMyLocationEnabled(true);
-                }
-            }
+    public void onConnected(@Nullable Bundle bundle) {
+        displayLocation();
+        if(mRequestLocationUpdates){
+            startLocationUpdates();
         }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocationCoordinates= location;
+        Toast.makeText(LIRRMap.this, "Location Changed", Toast.LENGTH_SHORT).show();
+        displayLocation();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG,"Connection failed" + connectionResult.getErrorMessage());
     }
 }
 
